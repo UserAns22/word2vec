0f667e2a05f6d3b77abc5dd0f06be369d5fb425c
UserAns22 <aniapplefan@gmail.com>
2017-10-31 08:28:39 -0700

    Created a TSNE Visualizer

--- TSNE_visualize.py
+++ TSNE_visualize.py
@@ -3,17 +3,14 @@ import numpy as np
 
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
-from sklearn.decomposition import IncrementalPCA
+#from sklearn.decomposition import IncrementalPCA
+
+import string
 import sys, warnings
 
-vector_dir = './vectors/'
+warnings.filterwarnings('ignore', '.*Unicode.*')
+
 plot_dir = './plots/'
-name = sys.argv[1]
-in_name = vector_dir +  name  + '.w2v'
-extension = '.png'
-full_model = Word2Vec.load(in_name)
-model = full_model.wv
-del full_model
 
 def normalize(vals):
     min_val = float(min(vals))
@@ -27,42 +24,70 @@ def normalize(vals):
         output.append(val)
     return output
 
-warnings.filterwarnings('ignore', '.*Unicode.*')
+def is_valid(word):
+    inv = string.punctuation
+    if any(c in inv for c in word):
+        return False
+    return True
 
-vectors = []
-labels = []
-for word in model.vocab:
-    vectors.append(model[word])
-    labels.append(word)
+def sort_wvc(wvc, max_count = None, max_words = None):
+    inv_count = lambda w: -1 * w[2]
+    if max_count:
+        lst = sorted([w for w in wvc if is_valid(w[0]) and w[2] < max_count], key=inv_count)
+    else:
+        lst = sorted([w for w in wvc if is_valid(w[0]) and w[2]], key=inv_count)
+    if max_words:
+        lst = lst[:max_words]
+    return lst
 
-print('- found ' + str(len(labels)) + ' entities x ' + str(len(vectors[0])) + ' dimensions')
-vectors = np.asarray(vectors)
-labels =  np.asarray(labels)
-
-# print('reducing dimensions')
-# ipca = IncrementalPCA(n_components = dimensions)
-# vectors = ipca.fit_transform(vectors)
-
-# vectors = np.asarray(vectors)
-
-tsne = TSNE(verbose = 2, n_components = 2, n_iter=50000)
-compute = 5000
-
-print('here we go')
-low_dim = tsne.fit_transform(vectors[:compute]) 
-print('we did it')
-plot = 500
-labels = labels[:plot]
-#Visualize
-
-filename = plot_dir + '{} TSNE.png'.format(name)
-plt.figure(figsize=(24,13.5))
-for i, label in enumerate(labels):
-    x,y = low_dim[i,:]
-    plt.scatter(x,y)
-    plt.annotate(label, xy = (x,y), xytext = (5,2), textcoords='offset points', ha='right', va = 'bottom')
+def visualize(kv, name, plot_dir=plot_dir, max_count = 250000):
     
+    wvc  = []#word vector count
+    for word in kv.vocab:
+        wvc.append([word, kv[word], kv.vocab[word].count]) 
 
-plt.show()
-plt.savefig(filename)
-print('whew')
+    dim = len(wvc[0][1])
+    sorted_wvc = list(zip(*sort_wvc(wvc, max_count)))
+
+    #debugging purposes
+    #return sorted_wvc
+#def blah(vectors, labels):
+    
+    vectors = np.asarray(sorted_wvc[1], dtype=np.float32)
+    labels =  np.asarray(sorted_wvc[0]) 
+
+
+    print('- found ' + str(len(labels)) + ' entities x ' + str(len(vectors[0])) + ' dimensions')
+    # print('reducing dimensions')
+    # ipca = IncrementalPCA(n_components = dimensions)
+    # vectors = ipca.fit_transform(vectors)
+    
+    # vectors = np.asarray(vectors)
+    
+    tsne = TSNE(verbose = 2, n_components = 2, n_iter=50000)
+    plot = 500
+    
+    low_dim = tsne.fit_transform(vectors[:plot]) 
+    labels = labels[:plot]
+    #Visualize
+    
+    filename = plot_dir + '{} TSNE.png'.format(name)
+    plt.figure(figsize=(24,13.5))
+    for i, label in enumerate(labels):
+        x,y = low_dim[i,:]
+        plt.scatter(x,y)
+        plt.annotate(label, xy = (x,y), xytext = (5,2), textcoords='offset points', ha='right', va = 'bottom')
+        
+    
+    plt.show()
+    plt.savefig(filename)
+
+if __name__ == "__main__":
+    vector_dir = './vectors/'
+    name = sys.argv[1]
+    in_name = vector_dir +  name  + '.w2v'
+    extension = '.png'
+    full_model = Word2Vec.load(in_name)
+    kv = full_model.wv
+    del full_model
+    visualize(kv, name)
